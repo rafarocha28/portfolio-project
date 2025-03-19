5fbff8c24ad6cbf823fac44884fd08b1fd27070c
Rafael Rocha <rafaelrocha@outlook.com>
2025-03-18 22:00:25 -0300

    feat: new modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { DbModule } from './db/db.module';
+import { ProjectModule } from './project/project.module';
+import { TechModule } from './tech/tech.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DbModule, ProjectModule, TechModule],
+  controllers: [AppController]
 })
 export class AppModule {}
